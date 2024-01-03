ad84723e34a22b1afa5ff509b62e49fa2690b080
Soumili Lahiri <155342372+Mili-sou@users.noreply.github.com>
2024-01-03 13:59:21 +0530

    Create life_in_weeks.py

---
+++ day 2 codes/life_in_weeks.py
@@ -0,0 +1,7 @@
+#The program will take your current age as the input and output a message with our time left if we live upto 90 years.
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+a=int(age)
+w=(90*52)-a*52
+print("You have",w,"weeks left.")
