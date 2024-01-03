ea97c22c15c84c0c5121a1091e813f52afcc1971
Soumili Lahiri <155342372+Mili-sou@users.noreply.github.com>
2024-01-03 13:54:56 +0530

    Update data_types.py

--- day 2 codes/data_types.py
+++ day 2 codes/data_types.py
@@ -1,1 +1,8 @@
-
+#Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+num=two_digit_number
+sum=int(num[0])+int(num[1])
+print(sum)
